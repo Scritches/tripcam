45b2b9221174c41561d43216b50aad192626ca60
Scritches <dzscritches@gmail.com>
2018-05-04 13:29:55 -0400

    Making the username edit a little more obvious
    and testing auto-refresh on disconnect + client version mismatch.

--- server/app/rooms.js
+++ server/app/rooms.js
@@ -107,7 +107,7 @@ class Room extends EventEmitter {
   handleConnect(username, client) {
     client.sendMessage({
       messageType: 'hello',
-      clientVersion: '0.1'
+      clientVersion: '0.2'
      });
     this.clients[client.clientId] = client;
   }
