40621f86b3ee610ff125ea3565ddb0d8a6bc3d01
Scritches <dzscritches@gmail.com>
2019-05-20 16:15:58 -0400

    Improved username visibility

--- server/app/rooms.js
+++ server/app/rooms.js
@@ -108,7 +108,7 @@ class Room extends EventEmitter {
   handleConnect(username, client) {
     client.sendMessage({
       messageType: 'hello',
-      clientVersion: '0.8'
+      clientVersion: '0.81'
      });
     this.clients[client.clientId] = client;
   }
