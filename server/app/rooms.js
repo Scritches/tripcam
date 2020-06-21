26e3ffac67b917914a853e2b97c6d0e794169b01
Scritches <dzscritches@gmail.com>
2020-06-20 21:02:03 -0400

    bump client version

--- server/app/rooms.js
+++ server/app/rooms.js
@@ -108,7 +108,7 @@ class Room extends EventEmitter {
   handleConnect(username, client) {
     client.sendMessage({
       messageType: 'hello',
-      clientVersion: '0.84'
+      clientVersion: '0.85'
      });
     this.clients[client.clientId] = client;
   }
